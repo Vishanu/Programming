a74736575ad8725c1b1b9a9eeeae76ae5c4e961f
Vishanu Giri <vishanugiri2002@gmail.com>
2026-03-20 10:39:15 +0530

    fibonacci

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -1,5 +1,21 @@
+
+import java.util.Scanner;
+
 class Fibonacci{
+    static int fibo(int n){
+        if(n<=1){
+            return n;
+        }
+        int last = fibo(n-1);
+        int slast = fibo(n-2);
+        return last + slast;
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibo(n));
     }
 }
+
+
+// TC = (2^n)
