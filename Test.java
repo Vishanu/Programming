64e85831698100a628dc8755ffecfbf4ab0ff919
Vishanu Giri <vishanugiri2002@gmail.com>
2026-04-14 19:14:55 +0530

    patternprogrammin started

---
+++ Test.java
@@ -0,0 +1,8 @@
+class Test { 
+    static { 
+        main(null); 
+    } 
+    public static void main(String[] args) { 
+        System.out.println("Hello"); 
+    } 
+} 
---
+++ practicejava/patternprogramming/Pattern1.java
@@ -0,0 +1,20 @@
+package practicejava.patternprogramming;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+    static void pattern1(int n){
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = sc.nextInt();
+        pattern1(n);
+    }
+}
