2b4efcbca18ace217bce5d0e739074810d4b536f
Vishanu Giri <vishanugiri2002@gmail.com>
2026-04-15 09:43:43 +0530

    Mini-Max Sum (Hackerrank)

--- practicejava/arraylist/MiniMaxSum.java
+++ practicejava/arraylist/MiniMaxSum.java
@@ -2,15 +2,8 @@
 
 package Arraylist;
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
 import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 class Result {
@@ -39,7 +32,7 @@ class Result {
 
 }
 
-public class miniMaxSum {
+public class MiniMaxSum {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
