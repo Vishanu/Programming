d34bd11c316860af3fdbd01137497a77b8131282
Vishanu Giri <vishanugiri2002@gmail.com>
2026-04-19 23:30:34 +0530

    242. Valid Anagram

---
+++ practicejava/18042026/ValidAnagram.java
@@ -0,0 +1,28 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        if(s.length() != t.length()) return false; 
+    
+        for(int i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            map1.put(ch , map1.getOrDefault(ch, 0)+1);
+        }
+        for(int i = 0; i<t.length(); i++){
+            char ch = t.charAt(i);
+            map2.put(ch , map2.getOrDefault(ch, 0)+1);
+        }
+
+        if(map1.equals(map2)) return true;
+        else return false;
+        
+    }
+}
+
+public class ValidAnagram {
+    
+}
