eac3158511316dc2ee943e82e4b6bd943fcd474b
Vishanu Giri <vishanugiri2002@gmail.com>
2026-04-26 19:33:13 +0530

    done

---
+++ practicejava/patternprogramming/Pyramid.java
@@ -0,0 +1,16 @@
+public class Pyramid {
+    public static void main(String[] args) {
+        int rows = 5; // Number of rows in the pyramid
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
